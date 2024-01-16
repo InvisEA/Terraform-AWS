40994d64d1326d322371f182c25cf2a33c1b0321
InvisEA <geniusser@mail.ru>
2024-01-16 23:52:29 +0300

    changed region of aws instances

--- 02-basics/aws-backend/main.tf
+++ 02-basics/aws-backend/main.tf
@@ -1,16 +1,16 @@
 terraform {
-  #############################################################
-  ## AFTER RUNNING TERRAFORM APPLY (WITH LOCAL BACKEND)
-  ## UNCOMMENT THIS CODE THEN RERUN TERRAFORM INIT TO
-  ## SWITCH FROM LOCAL BACKEND TO REMOTE AWS BACKEND
-  #############################################################
-  # backend "s3" {
-  #   bucket         = "devops-directive-tf-state" # REPLACE WITH YOUR BUCKET NAME
-  #   key            = "03-basics/import-bootstrap/terraform.tfstate"
-  #   region         = "us-east-1"
-  #   dynamodb_table = "terraform-state-locking"
-  #   encrypt        = true
-  # }
+  ############################################################
+  # AFTER RUNNING TERRAFORM APPLY (WITH LOCAL BACKEND)
+  # UNCOMMENT THIS CODE THEN RERUN TERRAFORM INIT TO
+  # SWITCH FROM LOCAL BACKEND TO REMOTE AWS BACKEND
+  ############################################################
+  backend "s3" {
+    bucket         = "terraform-practice-tf-state" # REPLACE WITH YOUR BUCKET NAME
+    key            = "02-basics/import-bootstrap/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-locking"
+    encrypt        = true
+  }
 
   required_providers {
     aws = {
